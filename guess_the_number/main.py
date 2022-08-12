34d3b21c88758cedd599587edfeeee933e4f6821
Eɢᴏʀ Gᴏʀʙᴜɴᴏᴠ <un.9bot@gmail.com>
2022-08-12 16:54:17 +0300

    Realize: validate input number
    add def is_valid()
    
    is_valid()
    - checking a number that it is really a number.
    - check that it lies between 1 and 100 (inclusive)

--- guess_the_number/main.py
+++ guess_the_number/main.py
@@ -1,6 +1,10 @@
 from random import randint
 
 
+def is_valid(in_num):
+    return in_num.isdigit() and 1 <= int(in_num) <= 100
+
+
 def lucky():
     generate_num = randint(1, 100)
     in_num = int(input('Угадайте число от 1 до 100 (включительно)\n'))
